45b15d0308d4ad5f04823f3fdad7721e65c08674
urbeingwatched8 <35970083+urbeingwatched8@users.noreply.github.com>
2020-12-23 17:04:57 +0300

    Update server.test.js

--- src/server.test.js
+++ src/server.test.js
@@ -1,10 +1,10 @@
-describe('This is just a dummy test 1', () => {
+describe('This is just test 1', () => {
   it('should test that true === true', () => {
     expect(true).toBe(true)
   })
 })
 
-describe('This is just a dummy test 2', () => {
+describe('This is just test 2', () => {
   it('should test that false === false', () => {
     expect(false).toBe(false)
   })
